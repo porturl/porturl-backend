8f5f5a2f0c9223f10fe02d3c7c98cf44f5cc8aa8
Bernhard Friedreich <friesoft@gmail.com>
2023-09-30 12:19:18 +0200

    change config prefix to porturl

--- src/main/java/org/friesoft/porturl/security/CustomSecurityConfiguration.java
+++ src/main/java/org/friesoft/porturl/security/CustomSecurityConfiguration.java
@@ -13,7 +13,7 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class CustomSecurityConfiguration {
 
-    @Value("${application.security.enabled:true}")
+    @Value("${porturl.security.enabled:true}")
     private boolean securityEnabled;
 
     @Bean
