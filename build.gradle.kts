e258bbe411ec484914b2b1ce89de05e6a45e2db7
Bernhard Friedreich <friesoft@gmail.com>
2025-08-04 12:26:19 +0200

    Update to spring boot 3.4, java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,18 @@ import net.researchgate.release.ReleaseExtension
 
 plugins {
     java
-    id("org.springframework.boot") version "3.2.4"
-    id("io.spring.dependency-management") version "1.1.4"
-    id("org.graalvm.buildtools.native") version "0.10.1"
-    id("net.researchgate.release") version "3.0.2"
+    id("org.springframework.boot") version "3.4.8"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("org.graalvm.buildtools.native") version "0.11.0"
+    id("net.researchgate.release") version "3.1.0"
+    id("com.github.ben-manes.versions") version "0.52.0"
 }
 
 group = "org.friesoft.porturl"
 version = "0.0.1-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 configurations {
@@ -35,12 +36,13 @@ dependencies {
     compileOnly("org.projectlombok:lombok")
 
     // logging
-    implementation("ch.qos.logback:logback-access")
+    implementation("ch.qos.logback.access:logback-access-common:2.0.6")
+    implementation("ch.qos.logback.access:logback-access-tomcat:2.0.6")
     implementation("ch.qos.logback:logback-classic")
     implementation("ch.qos.logback.contrib:logback-json-classic:0.1.5")
     implementation("ch.qos.logback.contrib:logback-jackson:0.1.5")
     implementation("com.fasterxml.jackson.core:jackson-databind")
-    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
+    implementation("net.logstash.logback:logstash-logback-encoder:8.1")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
